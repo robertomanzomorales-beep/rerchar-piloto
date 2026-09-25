50356512c21527267661dd0a80e62827b3353587
Roberto Manzo <robertomanzomorales@gmail.com>
2026-09-25 00:22:53 -0300

    Corregir despliegue en Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: { serverActions: { bodySizeLimit: process.env.VERCEL ? "4.3mb" : "6mb" } },
   serverExternalPackages: ["@electric-sql/pglite", "pg"],
+  outputFileTracingExcludes: {
+    "/*": ["./.local/**/*", "./.next/lock"],
+  },
 };
 
 export default nextConfig;
